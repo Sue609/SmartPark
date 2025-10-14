4cf38d2ad609aa6927a3a65c3c0e664da9865a60
Sue609 <122805048+Sue609@users.noreply.github.com>
2025-10-14 09:04:31 +0300

    Modifying the hash function, by capitalizing to make it easy to import it

--- internal/parking/hash.go
+++ internal/parking/hash.go
@@ -1,11 +1,11 @@
 package parking
 
-func hash(number_plate string, parking_lot []string) int {
+func Hash(number_plate string, table_size int) int {
 	sum := 0
 
 	for _, char := range number_plate {
 		sum += int(char)
 	}
 
-	return sum % len(parking_lot)
+	return sum % table_size
 }
